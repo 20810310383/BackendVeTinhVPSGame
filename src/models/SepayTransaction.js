6f91038e8f2d47ea3f25eeee9896791aeafdf548
Do Khac Tu <dokhactu1822@gmail.com>
2025-03-15 16:02:02 +0700

    up

--- src/models/SepayTransaction.js
+++ src/models/SepayTransaction.js
@@ -12,7 +12,9 @@ const SePayTransactionModel = new mongoose.Schema(
             description: { type: String },
             transferAmount: { type: Number },
             referenceCode: { type: String },
-            accumulated: { type: Number }
+            accumulated: { type: Number },            
+            bonusAmount: { type: Number },
+
       }
 );
 
