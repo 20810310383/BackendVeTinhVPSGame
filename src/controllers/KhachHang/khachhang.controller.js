48a5bfd55d428d7a3c86c05cbee09b0e7b027ed7
Do Khac Tu <dokhactu1822@gmail.com>
2025-03-24 23:35:27 +0700

    up

--- src/controllers/KhachHang/khachhang.controller.js
+++ src/controllers/KhachHang/khachhang.controller.js
@@ -253,7 +253,7 @@ module.exports = {
                 });
 
                 // const matchContent = sePayWebhookData.content.match(/NAP([a-f0-9]{24})/);
-                const matchContent = sePayWebhookData.content.match(/NAP([a-zA-Z0-9]{6,24})/);
+                const matchContent = sePayWebhookData.content.match(/NAP([a-zA-Z0-9]{1,24})/);
                 console.log("matchContent: ", matchContent);                
                 const idUser = matchContent[0].replace("NAP", "").toLowerCase();
                 console.log("idUser: ", idUser);                
